0fbc305dee29cbfaf780c33669a9424af22cb928
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-10-25 21:05:07 +0200

    adding socials

---
+++ html_advanced/blob.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+	int a = 3;
+	int b = a | 6;
+	printf("%d, %d\n", a, b);
+	return(0);
+}
---
+++ html_advanced/teste.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from lib2to3.pytree import _Results
+
+
+def uniq_add(my_list=[]):
+    result = sum()
+
+
+
+
+    uniq_add = __import__('2-uniq_add').uniq_add
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
